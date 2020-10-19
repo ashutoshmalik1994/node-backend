9204521499dd6f78a747a06995008b05df9f3a3d
ashutoshmalik1994 <ashutoshmalik.am@gmail.com>
2020-10-19 17:28:46 +0530

    commit for changing the gmail port

--- authorisation/authRoutesMethods.js
+++ authorisation/authRoutesMethods.js
@@ -10,12 +10,13 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
 const transporter = nodemailer.createTransport({
+    service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
-      user: 'ashutoshmalik.am@gmail.com',
-      pass: '8950603035'
+        user: 'ashutoshmalik.am@gmail.com',
+        pass: '8950603035'
     }
 });
 /************** User Registration API Start *************/
